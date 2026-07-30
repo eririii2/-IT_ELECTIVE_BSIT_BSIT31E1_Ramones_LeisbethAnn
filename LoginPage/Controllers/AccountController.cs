020d9c96ebc89a055dc9a6652c57360a30fdda29
eririii2 <leisrylle@gmail.com>
2026-07-30 09:09:30 +0800

    save1

---
+++ LoginPage/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginPage.Models;
+
+namespace LoginPage.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (model.Username == "admin" && model.Password == "123456")
+            {
+                return RedirectToAction("Welcome");
+            }
+
+            ModelState.AddModelError("", "Invalid username or password.");
+            return View(model);
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
